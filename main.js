297db458bdf35ba74793d5f361c880534c4d1278
SnCjI <july_sanchez11@outlook.com>
2024-12-14 15:48:10 -0600

    js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Funcion (Pedacito de código reutilizable) Una linea (Comentario)
 
 /* Comentario:
@@ -7,14 +7,3 @@ Linea */
 
 function redirigir(url){
     window.location.href = url;
-=======
-// Funcion (Pedacito de código reutilizable) Una linea (Comentario)
-
-/* Comentario:
-Multi 
-Linea */ 
-
-function redirigir(url){
-    window.location.href = url;
->>>>>>> fa24a77a9482df4fed49765d8b3e1906cb1447d9
-}
